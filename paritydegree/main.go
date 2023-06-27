9e88fb5eedf1a71ddc8f2ff613284929083c3877
betul <betbil@gmail.com>
2023-06-27 11:15:04 +0300

    paritydegree

---
+++ paritydegree/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func Solution(N int) int {
+	// Implement your solution here
+	//16 8 4 2 1
+	i := 0
+	for {
+		isOdd := (N & 1) == 1
+		if isOdd {
+			break
+		}
+		N = N >> 1
+		i++
+	}
+	return i
+}
+
+func main() {
+	num := 45
+	binary := strconv.FormatInt(int64(num), 2)
+	fmt.Println(binary)
+	fmt.Println(Solution(num))
+	num = 24
+	binary = strconv.FormatInt(int64(num), 2)
+	fmt.Println(binary)
+	fmt.Println(Solution(num))
+}
